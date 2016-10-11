e3b23f2c7c8f3bf9f42299a7f2f0e1f0e7cbc3f0
Edgar <edgar.mantes11@gmail.com>
2016-10-11 16:40:32 -0500

    Added the function to add new elements for the searched youtube videos

--- app.js
+++ app.js
@@ -13,16 +13,24 @@ function getDataFromApi(searchTerm, callback) {
 function displaySearchData(data) {
   var resultElement = '';
   if (data.items) {
+  	console.log(data.items);
     data.items.forEach(function(data) {
-     resultElement += '<li><a href="https://www.youtube.com/watch?v=' + data.id.videoId + '" target="_blank"><img src="' + data.snippet.thumbnails.medium.url + '"></a></li>';
-      
+     var templete = $('.col-4');
+     var new_child = $(templete[0]).clone();
+     console.log(new_child);
+     new_child.find('a').attr('href', data.id.videoId);
+    new_child.find('img').attr('src', data.snippet.thumbnails.medium.url);
+   // console.log(new_child);
+    $('.js-search-results').append(new_child);
+    //resultElement += <div class="col-4"> <div class="box rey"> <img src="images/rey_square.png"><div class="description"><h3 class="name">Ray</h3><p class="desc">Protagonist, from Tatooine</p></div></div></div>
+
     });
   }
   else {
     resultElement += '<p>No results</p>';
   }
   
-  $('.js-search-results').html(resultElement);
+  //$('.js-search-results').html(resultElement);
 }
 
 function watchSubmit() {
